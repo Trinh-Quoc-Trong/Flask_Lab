e1c3b42fad0cb5da6ca0e019e2a219bb8baa2327
Trinh-Quoc-Trong <trinhtrong2423@gmail.com>
2024-11-13 05:32:53 +0700

    add file 2

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ def hello_world():
 def user(name):
     return f'<h1>User is {name}</h1>'
 
+@app.route('/blog/<int:blog_int>')
+def blog(blog_int):
+    return f'<h1>Blog number {blog_int}</h1>'
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
